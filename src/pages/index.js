23129005aca637c790c8722c8ef20e114369fd83
matheus benites <=benites.amorim@gmail.com>
2020-04-10 16:51:33 -0300

    Iniciando a estrutura do novo Blog Papo de Dev

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>HOme</h1>
+  </Layout>
+)
+
+export default IndexPage
