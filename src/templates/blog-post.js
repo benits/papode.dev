1baeb36dcac2e2e4d4978c1dc6f4f3434f75036b
matheus benites <=benites.amorim@gmail.com>
2020-04-13 12:11:40 -0300

    componente RecommendedPost criado

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,14 +1,33 @@
 import React from "react"
 import { graphql } from "gatsby"
 
-const BlogPost = ({ data }) => {
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+
+import * as S from "../components/Post/styled"
+import RecommendedPosts from "../components/RecommendedPost"
+
+const BlogPost = ({ data, pageContext }) => {
   const post = data.markdownRemark
 
+  const next = pageContext.nextPost
+  const previous = pageContext.previousPost
+
   return (
-    <>
-      <h1>{post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-    </>
+    <Layout>
+      <SEO title={post.frontmatter.title} />
+      <S.PostHeader>
+        <S.PostDate>
+          {post.frontmatter.date} • {post.timeToRead} min de leitura
+        </S.PostDate>
+        <S.PostTitle>{post.frontmatter.title}</S.PostTitle>
+        <S.PostDescription>{post.frontmatter.description}</S.PostDescription>
+      </S.PostHeader>
+      <S.MainContent>
+        <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+      </S.MainContent>
+      <RecommendedPosts next={next} previous={previous} />
+    </Layout>
   )
 }
 
@@ -16,9 +35,12 @@ export const query = graphql`
   query Post($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       frontmatter {
+        date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+        description
         title
       }
       html
+      timeToRead
     }
   }
 `
