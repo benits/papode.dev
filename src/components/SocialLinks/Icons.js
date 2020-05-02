4341dcd4809f039c568b6c9d51d6b574043f7dee
matheus benites <=benites.amorim@gmail.com>
2020-05-02 14:12:19 -0300

    add youtube

--- src/components/SocialLinks/Icons.js
+++ src/components/SocialLinks/Icons.js
@@ -1,9 +1,11 @@
 import { Github } from "@styled-icons/boxicons-logos/Github"
 import { LinkedinSquare as Linkedin } from "@styled-icons/boxicons-logos/LinkedinSquare"
+import { Youtube } from "@styled-icons/boxicons-logos/Youtube"
 
 const Icons = {
   Github,
   Linkedin,
+  Youtube,
 }
 
 export default Icons
--- src/components/SocialLinks/content.js
+++ src/components/SocialLinks/content.js
@@ -7,6 +7,10 @@ const links = [
     label: "Linkedin",
     url: "https://www.linkedin.com/in/benites-amorim/",
   },
+  {
+    label: "Youtube",
+    url: "https://www.youtube.com/channel/UCFy-US4vCX_1TQt0Z1n6guA",
+  },
 ]
 
 export default links
